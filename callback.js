221a65356c33df5360f761623861229397204975
sushma bhapkar <sushmabhapkar244@gmail.com>
2024-01-13 13:58:51 +0530

    Await(TASK_N0=15)

---
+++ callback.js
@@ -0,0 +1,29 @@
+const posts = [
+    { title:'Post One', body:'This is the post one'},
+     { title:'Post Two', body:'This is the post two'}
+];
+
+function getpost()
+{
+    setTimeout(()=>{
+
+        let output = '';
+        posts.forEach((post,index)=>{
+            
+            output=output+`<li>${post.title}</li>`;
+ });
+ document.body.innerHTML=output;
+    },1000);
+}
+
+
+function createpost(post,callback)
+{
+
+setTimeout(()=>{
+
+posts.push(post);
+callback();
+},2000) ;   
+}
+createpost({ title:"Post Three" ,body:"This is the post Three"},getpost);
